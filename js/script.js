73edb14f6b80941e4178c8969509291e88c7db5b
Vladimir <dik2043@mail.ru>
2019-04-13 15:59:42 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var mainNav = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+
+navToggle.addEventListener('click', function () {
+    if (mainNav.classList.contains('main-nav--closed'))   {
+        mainNav.classList.remove('main-nav--closed');
+        mainNav.classList.add('main-nav--opened')
+    } else {
+        mainNav.classList.add('main-nav--closed');
+        mainNav.classList.remove("main-nav--opened")
+    }
+})
